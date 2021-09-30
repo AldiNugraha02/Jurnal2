3575c4821e472c6d58c0eb1419356c6eec85b916
AldiNugraha02 <nugrahaaldi689@gmail.com>
2021-09-30 21:11:43 +0700

    HelloWorld.java

---
+++ Tugas2/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+		//Komentar satu baris
+		 /komentar baru
+		 *adudu
+	    System.out.println("NAma:AldiNugraha!"); 
+		System.out.println("Nim:6706210144 !");
+		System.out.println("kelas:45-03 !");
+
+	}
+}	
